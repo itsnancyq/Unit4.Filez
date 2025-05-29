66dc2422ce78bcebf635ac86e54b37360f921321
Nancy Quinonez <nancyq29@hotmail.com>
2025-05-29 17:32:43 -0500

    updated

--- db/seed.js
+++ db/seed.js
@@ -6,5 +6,10 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  
+  for(let i=1; i <= 3; i++){
+    const folders = await createFolder( 'Folder ' + i )
+    for (let j=1; j <= 5; j++){
+      const files = await createFiles( 'File ' + j, 17 * j, folders.id )
+    }
+  }
 }
